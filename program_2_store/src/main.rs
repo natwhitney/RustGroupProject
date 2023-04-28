d08d1d3a9a90467521a29cb59b6fbf9ce7bc59f0
Fallenangel700 <90366792+Fallenangel700@users.noreply.github.com>
2023-04-27 23:40:56 -0500

    Program 2 work

---
+++ program_2_store/src/main.rs
@@ -0,0 +1,68 @@
+//Program 2 Mock Store
+/*
+    David Sullivan
+    4/25/23
+    An extremely simple store front that lets the user add items to a cart.
+*/
+use std::vec::Vec;
+
+fn main() {
+    struct Item{
+        name: String,
+        color: String,
+        size: String,
+    }
+    let mut cart: Vec<Item> = Vec::new();
+    let mut input = 0;
+    let mut nameIn;
+    let mut colorIn;
+    let mut sizeIn;
+
+  //  let mut test: Item = Item{
+  //      name: String::from("test"),
+  //      color: String::from("test"),
+  //      size: String::from("test")
+  //  };
+    
+    println!("Welcome to the Mock Store!");
+
+    while(input != 3)
+    {
+        println!("Select an option:");
+        println!("1: Add item to cart.");
+        println!("2: View cart.");
+        println!("3: Quit");
+        io:stdin().read_line( &mut input).expect("Didn't receive input.");
+
+        if(input == 1)
+        {
+            println!("What is the name of your item?");
+            io:stdin().read_line( &mut name).expect("Didn't receive input.");
+            println!("What is the color of your item?");
+            io:stdin().read_line( &mut color).expect("Didn't receive input.");
+            println!("What is the size of your item?");
+            io:stdin().read_line( &mut size).expect("Didn't receive input.");
+
+            
+            let tempItem: Item = Item{
+                nameIn: String::from("test"),
+                colorIn: String::from("test"),
+                sizeIn: String::from("test")
+            };
+            cart.push(tempItem);
+        }
+        else if(input == 2)
+        {
+            for i in &mut cart
+            {
+                println!("Item {} in cart:", x);
+                println!("Item : {}", cart[x].name);
+                println!("Color : {}", cart[x].color);
+                println!("Size : {}", cart[x].size);
+            }
+        }
+    }
+
+     println!("Ending program. Goodbye!");
+
+}
---
+++ program_2_store/target/debug/deps/program_2_store.d
@@ -0,0 +1,5 @@
+C:\Users\dzsfa\Desktop\School\Programming Languages\projectGitFolder\RustGroupProject\program_2_store\target\debug\deps\program_2_store.exe: src\main.rs
+
+C:\Users\dzsfa\Desktop\School\Programming Languages\projectGitFolder\RustGroupProject\program_2_store\target\debug\deps\program_2_store.d: src\main.rs
+
+src\main.rs:
